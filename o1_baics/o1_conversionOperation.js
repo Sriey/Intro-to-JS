db74f2d594c65ead406e5e584933bb0cb4dc6b15
Sriey <131192690+Sriey@users.noreply.github.com>
2025-06-11 09:30:13 +0000

    operations

---
+++ o1_baics/o1_conversionOperation.js
@@ -0,0 +1,27 @@
+let value =3
+let negValue =-value;
+console.log(negValue);
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/3);
+console.log(2%3);
+let str1="hello";
+let str2="Srijan"
+let str3= str1+str2
+console.log(str3);
+console.log(1+"2");
+console.log(1 + 2+"2");//output 122
+console.log("1"+ 2 + 2);//output 32
+// the abstract operation is taken as input ane the prefferedType is
+// decided based on which is coming first
+ console.log(+true)//this will give 1 becaue true is converted to no. for 
+ // inceremetnt
+console.log(+"");// this will give 0 
+let num1,num2, num3
+num1=num2=num3=100
+let gameCounter;
+++gameCounter
+console.log(gameCounter);
+//read from MDN js for prefix and suffix
