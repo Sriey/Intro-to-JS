1fc86700e95700fac7c929f8255217a18a84266b
Sriey <131192690+Sriey@users.noreply.github.com>
2025-06-11 06:23:22 +0000

    variables

---
+++ o1_baics/o1_variables.js
@@ -0,0 +1,21 @@
+const accountId =144453
+let accountEmail ="srijanpandey07@gmail.com"
+var accountPassword="123345"
+accountCity="Jaipur"
+
+// accountId=2 not allowed since already defined as const
+accountEmail="sp@gmail.com"
+accountCity="Bengaluru"
+accountPassword="qwert"
+//this is declaration for an undefined variable which is correct
+let accountState;
+console.log(accountEmail);
+console.log(accountId);
+console.log(accountPassword);
+console.log(accountCity);
+console.log(accountState);
+
+console.table[accountEmail,accountId,accountPassword,accountCity,accountState]
+
+/*do not use var as it suffer from block and functional scope boundary disparity*/
+ 
