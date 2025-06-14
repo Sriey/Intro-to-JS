ebca53c41cf779e86a6a1031a244545eb4981569
Sriey <131192690+Sriey@users.noreply.github.com>
2025-06-14 05:17:30 +0000

    nums

---
+++ o1_baics/o9_number&math.js
@@ -0,0 +1,30 @@
+// const score =400
+// const balance =new Number(100);
+// console.log(balance);
+// console.log(score);
+
+// console.log(balance.toString());
+// //now number is a steing so we get string mtd
+// console.log(balance.toFixed(1));
+// const otherNumber =23.6523
+// console.log(otherNumber.toPrecision(3));
+// const hundred =100000
+
+//+++++++++++++++++++++++++++++++++++++++++++++
+console.log(Math);
+console.log(Math.abs(4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(3.9));
+console.log(Math.sqrt(4));
+console.log((Math.max(4,3,2,1)));
+
+//imp  
+console.log(Math.random());
+//output of random is always between [0,1] closed
+console.log(Math.floor((Math.random()*10) +1));
+//
+const min = 10
+const max = 20
+//formula for number in range
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
